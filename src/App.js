8c6e8d9c8571ac7918d88fc8d00a0200587e2ee3
siwarchebchoub01 <siwarchebchoub02@gmail.com>
2023-08-24 19:35:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import  Card from'./card';
+import PlayersList from './playerlist';
 import Navv from "./navbar";
-import Card2 from './Card2';
-import Card3 from './Card3';
+ 
 
 function App() {
   return (
     <div className="App">
- 
-   <Navv/>
+    <Navv/>
    <div className="prt">
-    <Card/>
-    
-    <Card2/>
-    
-    <Card3/>
+    <PlayersList/>
     </div>
     </div>
   );
